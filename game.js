aac6346842dde9e585dbfa5108e4b6333413f325
Adam Miller <chadam189@gmail.com>
2018-01-08 14:54:36 -0800

    Init commit

---
+++ game.js
@@ -0,0 +1,16 @@
+/* 
+
+Tic tac toe
+
+Classes: Board
+
+Display Board
+
+Helpers: getNextMove, hasPlayerWon, isMoveValid
+
+prompt alternating players for inputs
+	validate their move - if not valid, ask them to redo
+	check for win condition
+		if win condition reached, announce winner and restart
+
+*/
