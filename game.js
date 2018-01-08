27dae6256f84455441cf30f2ba69ea15aa6295bb
Adam Miller <chadam189@gmail.com>
2018-01-08 15:29:37 -0800

    Outline helper functions and overall game flow

--- game.js
+++ game.js
@@ -15,11 +15,42 @@ prompt alternating players for inputs
 
 class Board {
   constructor () {
-  	// create 3x3 board
-  	this.board = new Array(3).fill(new Array(3).fill(0));
+  	// create 3x3 board of 
+  	this.board = new Array(3).fill(new Array(3).fill(' - '));
   }
 }
 
 const board = new Board();
 
-// console.log(JSON.stringify(board, null, 2));
+// game state
+
+
+// welcome players
+
+console.log('Welcome to Tic Tac Toe! \nPlayer 1 will have X\'s, and goes first. Player 2 will have O\'s, and will go second.')
+
+let isGameComplete = false;
+let currentPlayer = 1;
+
+	// while nobody has won (or a player hasn't selected 'quit') then continue
+
+while (!isGameComplete) {
+	// get move from next player
+	const nextMove = getNextMove(currentPlayer);
+  
+  if (nextMove === 'q') {
+  	isGameComplete = true;
+  	continue;
+  }
+	// update board with the valid move
+	updateBoard(currentPlayer, nextMove);
+
+  // end game if the current player made a move to win
+	if (hasPlayerWon(currentPlayer)) {
+		console.log(`Congrats to Player ${currentPlayer} for winning!`);
+		isGameComplete = true;
+	} 
+}
+
+console.log('thanks for playing!')
+
--- helpers.js
+++ helpers.js
@@ -13,3 +13,34 @@ const displayBoard = (board) => {
   }
   return table;
 };
+
+const getNexMove = (playerNumber) => {
+  let move = prompt(`Player ${player}, please select the spot of your next move (or put \'q\' to quit)`);
+  // check whether move is valid
+
+  while (!isMoveValid(board, move)) {
+  	move = prompt(`That is an invalid move; another piece is in that spot. Please select a valid spot for your next move (or put \'q\' to quit)`);
+  }
+  // return valid move
+  return move;
+};
+
+const updateBoard = (player, spot) => {
+
+	// change spot on board from ' - ' to either X (for Player 1) or ' O  ' for Player 2
+
+	
+}
+
+const hasPlayerWon = (board, player) => {
+
+	// player param is to otimize searches to only check if the player who just placed a piece won; no need to check if their opponent won this turn
+
+	// return true if yes
+}
+
+const isMoveValid = (board, spot) => {
+	// check to see if that spot is qual to: ' - '
+	// if so, then we're good to place the piece
+	// if not, then that means another piece is already there - doesn't matter if it belons to the player or their opponent
+};
