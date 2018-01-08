efd0f9929cbeb175492ed0598167271c9d64514b
Adam Miller <chadam189@gmail.com>
2018-01-08 15:10:31 -0800

    Refactor displayBoard

--- helpers.js
+++ helpers.js
@@ -5,10 +5,11 @@
 
 
 const displayBoard = (board) => {
-	let table = '';
-	for (let i = 0; i < board.length; i++) {
-		// print row of board
-		table += `${JSON.stringify(board[i])}\n`;
-	}
-	return table;
+  let table = '';
+  for (let i = 0; i < board.board.length; i++) {
+    // print row of board
+    table += JSON.stringify(board.board[i]);
+    table += '\n ';
+  }
+  return table;
 };
